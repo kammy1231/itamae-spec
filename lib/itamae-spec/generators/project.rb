b01af2bacbf09e4edd8c244c930c78b72dc72429
kammy <kammy@kammy-no-MacBook-Pro.local>
2018-03-14 17:05:53 +0900

    Improve bundle install when create new project

--- lib/itamae-spec/generators/project.rb
+++ lib/itamae-spec/generators/project.rb
@@ -5,6 +5,12 @@ module Itamae
       def self.source_root
         File.dirname(__FILE__) + '/templates/project'
       end
+
+      def bundle
+        Dir.chdir(destination_root) do
+          run 'bundle install'
+        end
+      end
     end
   end
 end
